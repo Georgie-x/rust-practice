4f98836e665edab9fd753862a94309c046219276
Georgie <georgiex@gmail.com>
2025-10-13 21:19:44 +0100

    got lines printing withouth recursion bit

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+
+
+fn main() {
+    let gifts = vec![
+        "a partridge in a pear tree!",
+        "Two turtle doves, and",
+        "Three French hens",
+        "Four calling birds",
+        "Five gold rings",
+        "Six geese a-laying",
+        "Seven swans a-swimming",
+        "Eight maids a-milking",
+        "Nine ladies dancing",
+        "Ten lords a-leaping",
+        "Eleven pipers piping",
+        "Twelve drummers drumming",
+    ];
+
+    let ordinal = vec![
+        "first",
+        "second",
+        "third",
+        "fourth",
+        "fifth",
+        "sixth",
+        "seventh",
+        "eighth",
+        "ninth",
+        "tenth",
+        "eleventh",
+        "twelfth"
+    ];
+
+    
+
+    for i in 1..13 {
+    println!("On the {} day of Christmas my true love gave to me {}", ordinal[i -1], gifts[i -1]);
+
+    }
+}
