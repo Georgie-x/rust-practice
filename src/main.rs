4554def76487adeed7b80ce94839a1b36a9c4d27
Georgie <georgiex@gmail.com>
2025-10-14 21:18:13 +0100

    working with bad grammar and format

--- src/main.rs
+++ src/main.rs
@@ -31,10 +31,14 @@ fn main() {
         "twelfth"
     ];
 
-    
+    let mut previous_lines: Vec<String> = vec![];
 
     for i in 1..13 {
-    println!("On the {} day of Christmas my true love gave to me {}", ordinal[i -1], gifts[i -1]);
+        println!("On the {} day of Christmas my true love gave to me...", ordinal[i -1]);
+    let formatted_line = format!("{}", gifts[i -1]);     
+        println!("{}", formatted_line);
+        println!("{}", previous_lines.join(" "));
+        previous_lines.insert(0, formatted_line);
 
     }
 }
